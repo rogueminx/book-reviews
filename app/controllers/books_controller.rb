fd81670a15dd34931be387f77dbed8d8c0a46890
Anna Marie Cooper and Ron Craig <ron@>
2018-04-19 10:08:33 -0700

    add book update functionality, jooooy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,23 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = Book.find(params[:id])
+    user_id = @book.user_id
+    @user = User.find(user_id)
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update!(book_params)
+      flash[:notice] = "Joy! Your book was updated!"
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+
 private
   def book_params
     params.require(:book).permit(:author, :title, :genre, :rating, :featured)
